256aa069d790dfaa3cbbc227f724ba1cb1ae5c01
PolztiukVolodymyr <92870556+PolztiukVolodymyr@users.noreply.github.com>
2022-08-29 16:30:19 +0000

    Deploying to gh-pages from @ PolztiukVolodymyr/goit-react-hw-08-phonebook@1fa97a486bbdcf03932670858f11d28853086987 🚀

--- static/js/680.495fa9d0.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[680],{680:function(e,n,o){o.r(n),o.d(n,{default:function(){return r}});var t=o(184);function r(){return(0,t.jsx)(t.Fragment,{children:(0,t.jsx)("h2",{children:"Welcome to Phone Book"})})}}}]);
-//# sourceMappingURL=680.495fa9d0.chunk.js.map
